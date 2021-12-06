b4d55b5f038bb21eca83906cc32c09ae83f828fa
NGUYEN THI KIEU DIEM <n-diem@plott.local>
2021-12-06 13:01:53 +0700

    CRUD basic

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -1,13 +1,54 @@
 <?php
 class IndexController extends Zend_Controller_Action{
 
-    public function init(){
+    protected $_arrParam;
+    protected $_currentController;
+    protected $_actionMain;
 
+    public function init(){
+        $this->_arrParam = $this->_request->getParams();
+        $this->_currentController = '/'.$this->_arrParam['module'].'/'.$this->_arrParam['controller'];
+        $this->_actionMain = '/'.$this->_arrParam['module'].'/'.$this->_arrParam['controller'].'/index';
+
+        $this->view->arrParam = $this->_arrParam;
+        $this->view->currentController = $this->_currentController;
+        $this->view->actionMain = $this->_actionMain;
     }
 
     public function indexAction(){
-        echo '<br>'.__METHOD__;
         $model = new Model_Product();
-      
+        $this->view->ListItems = $model->listItem();
+    }
+
+    public function detailAction(){
+        $model = new Model_Product();
+        $this->view->item = $model->getItemDetail($this->_arrParam);
+    }
+
+    public function addAction(){
+        if($this->_request->isPost()){
+            $model = new Model_Product();
+            $model->addItem($this->_arrParam);
+            $this->redirect($this->_actionMain);
+        }
+    }
+
+    public function editAction(){
+        $model = new Model_Product();
+        $this->view->item = $model->getItemDetail($this->_arrParam);
+        if($this->_request->isPost()) {
+            $model->editItem($this->_arrParam);
+            $this->redirect($this->_actionMain);
+        }
+    }
+
+    public function deleteAction(){
+        $model = new Model_Product();
+        try{
+            $model->deleteItem($this->_arrParam);
+        }catch (Exception $e) {
+
+        }
+        $this->redirect($this->_actionMain);
     }
 }
--- application/modules/default/models/Product.php
+++ application/modules/default/models/Product.php
@@ -1,9 +1,45 @@
 <?php
 class Model_Product extends Zend_Db_Table{
-    protected $_name = 'product';
-    protected $_primary = 'id';
+    protected $_name = 'actor';
+    protected $_primary = 'actor_id';
 
     public function test(){
         echo '<br>'.__METHOD__;
+        echo '<br> Model';
+    }
+
+    public function listItem(){
+        $where = 'actor_id > 180';
+        $order = ' actor_id DESC';
+        $list_result = $this->fetchAll($where, $order)->toArray();
+        return $list_result;
+    }
+
+    public function getItemDetail($arrParam){
+        $where = 'actor_id = '.$arrParam['id'];
+        $detail = $this->fetchRow($where);
+        return $detail;
+    }
+
+    public function addItem($arrParam){
+        $row = $this->fetchNew();
+        $row->first_name = $arrParam['first_name'];
+        $row->last_name = $arrParam['last_name'];
+        $row->last_update = "2021-12-06 11:00:30";
+        $row->save();
+    }
+
+    public function editItem($arrParam){
+        $where = 'actor_id = '.$arrParam['id'];
+        $row = $this->fetchRow($where);
+        $row->first_name = $arrParam['first_name'];
+        $row->last_name = $arrParam['last_name'];
+        $row->last_update = "2021-12-06 11:00:30";
+        $row->save();
+    }
+
+    public function deleteItem($arrParam){
+        $where = 'actor_id = '.$arrParam['id'];
+        $this->delete($where);
     }
 }
